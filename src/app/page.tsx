88daf68b5dc3306ac192e31b863f692f8ed2c78d
ist197124 <jiaqi.yu@tecnico.ulisboa.pt>
2024-08-19 11:52:34 +0100

    Home page is done, but i do not why the animation is not work, i will think it later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
-import { Check, Star } from '../../node_modules/lucide-react'
+import { ArrowRight, Check, Star } from '../../node_modules/lucide-react'
 import Phone from "@/components/Phone";
 import { Icons } from "@/components/Icons";
+import { Reviews } from "@/components/Reviews";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -117,8 +121,8 @@ export default function Home() {
               </div>
 
             </div>
-             {/* second user review */}
-             <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+            {/* second user review */}
+            <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
               <div className='flex gap-0.5 mb-2'>
                 <Star className='h-5 w-5 text-green-600 fill-green-600' />
                 <Star className='h-5 w-5 text-green-600 fill-green-600' />
@@ -128,7 +132,7 @@ export default function Home() {
               </div>
               <div className='text-lg leading-8'>
                 <p>
-                &quot;I usually keep my phone together with my keys in my pocket
+                  &quot;I usually keep my phone together with my keys in my pocket
                   and that led to some pretty heavy scratchmarks on all of my
                   last phone cases. This one, besides a barely noticeable
                   scratch on the corner,{' '}
@@ -155,6 +159,75 @@ export default function Home() {
             </div>
           </div>
         </MaxWidthWrapper>
+        <div className="pt-16">
+          <Reviews />
+        </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className='py-24'>
+          <div className='mb-12 px-6 lg:px-8'>
+            <div className='mx-auto max-w-2xl sm:text-center'>
+              <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
+                Upload your photo and get{' '}
+                <span className='relative px-2 bg-green-600 text-white'>
+                  your own case
+                </span>{' '}
+                now
+              </h2>
+            </div>
+          </div>
+
+          <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+            <div className='relative flex flex-col items-center md:grid grid-cols-2 gap-40'>
+              <img
+                src='/arrow.png'
+                className='absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0'
+              />
+
+              <div className='relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl'>
+                <img
+                  src='/horse.jpg'
+                  className='rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full'
+                />
+              </div>
+
+              <Phone className='w-60' imgSrc='/horse_phone.jpg' />
+            </div>
+          </div>
+          <ul className='mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit'>
+            <li className='w-fit'>
+              <Check className='h-5 w-5 text-green-600 inline mr-1.5' />
+              High-quality silicone material
+            </li>
+            <li className='w-fit'>
+              <Check className='h-5 w-5 text-green-600 inline mr-1.5' />
+              Scratch- and fingerprint resistant coating
+            </li>
+            <li className='w-fit'>
+              <Check className='h-5 w-5 text-green-600 inline mr-1.5' />
+              Wireless charging compatible
+            </li>
+            <li className='w-fit'>
+              <Check className='h-5 w-5 text-green-600 inline mr-1.5' />5 year
+              print warranty
+            </li>
+
+            <div className='flex justify-center'>
+              <Link
+                className={buttonVariants({
+                  size: 'lg',
+                  className: 'mx-auto mt-8',
+                })}
+                href='/configure/upload'>
+                Create your case now <ArrowRight className='h-4 w-4 ml-1.5' />
+              </Link>
+            </div>
+          </ul>
+        </MaxWidthWrapper>
+      </section>
+      <section>
+        <Footer/>
       </section>
     </div>
   );
